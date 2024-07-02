99b09957a5e663aaf5ea59b28d9484521cb0f64f
Matheus Bassi <bassi.matheus@outlook.com>
2024-07-01 22:45:59 -0300

    initial config

---
+++ src/main/java/dev/bassi/picpay/picpaychallenge/PicpayChallengeApplication.java
@@ -0,0 +1,13 @@
+package dev.bassi.picpay.picpaychallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayChallengeApplication.class, args);
+	}
+
+}
