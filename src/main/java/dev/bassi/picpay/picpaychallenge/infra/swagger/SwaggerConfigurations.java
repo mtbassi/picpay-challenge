fb46269e4112d744beb0ced1aaae2fb5b53fa573
Matheus Bassi <bassi.matheus@outlook.com>
2024-07-02 00:48:30 -0300

    feat: implement swagger

---
+++ src/main/java/dev/bassi/picpay/picpaychallenge/infra/swagger/SwaggerConfigurations.java
@@ -0,0 +1,38 @@
+package dev.bassi.picpay.picpaychallenge.infra.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfigurations {
+
+    @Value("${swagger.application.name}")
+    private String applicationName;
+
+    @Value("${swagger.application.version}")
+    private String applicationVersion;
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().info(this.getInfo());
+    }
+
+    private Info getInfo() {
+        return new Info().title(applicationName)
+                .description("Simple bank transfer api.")
+                .version(applicationVersion)
+                .license(new License().name("Apache 2.0")
+                        .url("https://www.apache.org/licenses/LICENSE-2.0"))
+                .contact(this.getContact());
+    }
+
+    private Contact getContact() {
+        return new Contact().name("Matheus Bassi")
+                .url("https://www.linkedin.com/in/matheusbassi/");
+    }
+}
